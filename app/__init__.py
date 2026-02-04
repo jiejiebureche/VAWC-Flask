e6beee7d4f8225d4f7ff1ca9544755df2746f686
jiejiebureche <149884339+jiejiebureche@users.noreply.github.com>
2026-02-05 02:12:55 +0800

    create git ignore file

--- app/__init__.py
+++ app/__init__.py
@@ -14,8 +14,9 @@ def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
     
+    db.init_app(app)
+    bcrypt.init_app(app)
     
     migrate = Migrate(app, db)
     
